2fc1a9fa38a68e49e52508bc04cd34e44a0fc5a7
杨晓辉 <yangxh108@126.com>
2017-07-28 14:11:46 +0800

    create

---
+++ app/src/main/java/cn/xavier/zhzl/MainActivity.java
@@ -0,0 +1,14 @@
+package cn.xavier.zhzl;
+
+import android.os.Bundle;
+
+import cn.xavier.base.ui.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
